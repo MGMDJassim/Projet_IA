a2c842e7b33f0d8a972a570ff43d35f96ece5339
Jassim <jassim91@outlook.fr>
2025-03-04 13:19:13 +0100

    amélioration de la fonction initialisation

--- algo_gen.py
+++ algo_gen.py
@@ -1,12 +1,22 @@
 import random
-from Individu import Individu
+from ville import Ville
 new_pop = []
+villes = []
 
+def initailisation_des_villes(nbr_villes):
+    for i in range(nbr_villes):
+        villes.append(Ville(random.randint(0, 100), random.randint(0, 100)))
+    return villes
 
-def initialisation(taille):
-    for _ in range(taille):
-        new_pop.append(Individu(random.randint(0, 100), random.randint(0, 100)))
+def initialisation(taille, start, end):
+    for i in range(taille):
+        individu = villes[:]
+        random.shuffle(individu)
+        individu.insert(0, start)  # Ajouter le point de départ au début
+        individu.append(end)       # Ajouter la destination à la fin
+        new_pop.append(individu)
     return new_pop
+
 def selection(new_pop):
     pass
 
@@ -28,8 +38,21 @@ def algo_gen():
         enfant = mutation(enfant)
         pop = formation(pop, enfant)
 
-initialisation(10)
+taille = 10
 
+initailisation_des_villes(taille)
+
+point_de_depart = Ville(0, 0)
+destination = Ville(100, 100)
+initialisation(taille, point_de_depart, destination)
+
+print(villes)
+
+for i, individu in enumerate(new_pop):
+    print(f"Individu {i + 1}:")
+    for ville in individu:
+        print(ville)
+    print()  # L
 """
 Algo : 
 new_pop <- ensemble aléatoie d'individus
