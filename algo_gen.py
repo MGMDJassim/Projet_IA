2ee0ed892cf2888da68fb806ff7a677e86a6c68f
Jassim <jassim91@outlook.fr>
2025-03-06 16:19:54 +0100

    ajout des fonctions

--- algo_gen.py
+++ algo_gen.py
@@ -30,16 +30,8 @@ def evaluation(individus):
 # Selection des individus
 def selection(new_pop):
     evaluated = evaluation(new_pop)
-    pop_selct = [new_pop[evaluated[i][0]] for i in range(len(evaluated) // 2)]
-    
-    print("Population sélectionnée :")
-    for pop in pop_selct:
-        print(f"Évaluation = {total_distance(pop)}")
-        for ville in pop:
-            print(ville)
-        print()
-    
-    return pop_selct
+    pop_select = [new_pop[evaluated[i][0]] for i in range(len(evaluated) // 2)]
+    return pop_select
 
 # Recombinaison des parents
 def recombinasion(parents):
@@ -57,21 +49,18 @@ def recombinasion(parents):
                 enfant[i].append(ville)
     return enfant
 
-# Mutation des individus
+# Mutation
 def mutation(enfant):
-    mutation_rate = 0.01  # Taux de mutation
-    # Itère sur chaque ville dans l'itinéraire
+    mutation_rate = 0.05  
     for swapped in range(len(enfant)):
-        # Vérifie si une mutation doit se produire, basé sur le taux de mutation
         if random.random() < mutation_rate:
-            # Sélectionne une deuxième ville aléatoire dans l'itinéraire pour l'échange
             swap_with = int(random.random() * len(enfant))
-            # Échange les deux villes
             city1 = enfant[swapped]
             city2 = enfant[swap_with]
             enfant[swapped] = city2
             enfant[swap_with] = city1
-    return enfant  # Retourne l'itinéraire muté
+    return enfant 
+
 
 # Formation de la nouvelle population
 def formation(pop, enfant):
@@ -90,6 +79,11 @@ def algo_gen():
         enfant = recombinasion(parents)
         enfant = mutation(enfant)
         pop = formation(new_pop, enfant)
+
+
+# Test
+initailisation_des_villes(10)
+new_pop = initialisation(10)
 
 """
 Algo : 
