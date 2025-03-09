0b5f9a69eb885d47b8da196926275276e280dded
Jassim <jassim91@outlook.fr>
2025-03-09 15:21:34 +0100

    ajout de l'interface

--- algo_gen.py
+++ algo_gen.py
@@ -5,95 +5,41 @@ from ville import Ville
 new_pop = []
 villes = []
 
-def initailisation_des_villes(nbr_villes):
-    for i in range(nbr_villes):
-        villes.append(Ville(random.randint(0, 100), random.randint(0, 100)))
+def initialisation_des_villes(nbr_villes):
+    global villes
+    villes = [Ville(random.randint(0, 50), random.randint(0, 50)) for _ in range(nbr_villes)]
     return villes
 
 def initialisation(taille):
-    for i in range(taille):
-        individu = villes[:]
-        random.shuffle(individu)
-        new_pop.append(individu)
-    return new_pop
+    return [random.sample(villes, len(villes)) for _ in range(taille)]
 
 def total_distance(route):
-    distance = 0
-    for i in range(len(route) - 1):
-        distance += route[i].distance(route[i + 1])
-    distance += route[-1].distance(route[0])
-    return distance
+    return sum(route[i].distance(route[i + 1]) for i in range(len(route) - 1)) + route[-1].distance(route[0])
 
 def evaluation(individus):
-    evaluated = []
-    for i in range(len(individus)):
-        evaluated.append((i, total_distance(individus[i])))
-    evaluated.sort(key=lambda x: x[1])
-    return evaluated
+    return sorted(individus, key=total_distance)
 
-def selection(new_pop):
-    evaluated = evaluation(new_pop)
-    pop_select = []
-    for i in range(len(new_pop) // 2):
-        pop_select.append(new_pop[evaluated[i][0]])
-    return pop_select
+def selection(pop):
+    return evaluation(pop)[:len(pop) // 2]
 
 def recombinaison(parents):
     enfant = []
     for i in range(0, len(parents) - 1, 2):
-        parent1 = parents[i]
-        parent2 = parents[i + 1]
+        parent1, parent2 = parents[i], parents[i + 1]
         cut = len(parent1) // 2
         child1 = parent1[:cut] + [city for city in parent2 if city not in parent1[:cut]]
         child2 = parent2[:cut] + [city for city in parent1 if city not in parent2[:cut]]
-        enfant.append(child1)
-        enfant.append(child2)
+        enfant.extend([child1, child2])
     return enfant
 
-def mutation(enfant):
-    mutation_rate = 0.05  
-    for swapped in range(len(enfant)):
+def mutation(enfant, mutation_rate=0.05):
+    for route in enfant:
         if random.random() < mutation_rate:
-            swap_with = int(random.random() * len(enfant))
-            city1 = enfant[swapped]
-            city2 = enfant[swap_with]
-            enfant[swapped] = city2
-            enfant[swap_with] = city1
+            i, j = random.sample(range(len(route)), 2)
+            route[i], route[j] = route[j], route[i]
     return enfant
 
 def formation(pop, enfant):
-    nouvelle_pop = []
-    point = len(pop) // 2
-    for e in enfant:
-        if e not in pop:
-            nouvelle_pop.append(e)
-    nouvelle_pop = nouvelle_pop + pop[:point]
+    nouvelle_pop = enfant + pop[:len(pop)//2]  # Garde une partie des anciens
     random.shuffle(nouvelle_pop)
-    return nouvelle_pop
-
-def algo_gen():
-    initailisation_des_villes(10)
-    new_pop = initialisation(10)
-    while True:
-        parents = selection(new_pop)
-        enfant = recombinaison(parents)
-        enfant = mutation(enfant)
-        new_pop = formation(new_pop, enfant)
-
-
-
-# Test
-# algo_gen()
-
-
-
-"""
-Algo : 
-new_pop <- ensemble aléatoire d'individus
-Répéter :
-| Parent <- selection (pop)
-| enfant <- recombinaison(Parent)
-| enfant <- mutation(enfant)
-| pop <- Formation (pop, enfant)
-tant qu'on améliore la valeur des individus
-"""
+    return nouvelle_pop[:len(pop)]
--- interface.py
+++ interface.py
@@ -1,51 +1,89 @@
-from matplotlib import pyplot as plt
-from algo_gen import *
+import sys
+import random
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QGraphicsView, QGraphicsScene, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QPen, QBrush
+from algo_gen import initialisation_des_villes, total_distance, selection, recombinaison, mutation, formation
 
-def plot_data(data):
+class GraphicsView(QGraphicsView):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
 
-    x = [city.x for city in data]
-    y = [city.y for city in data]
-    
-    plt.scatter(x, y, c='blue', marker='o')
-    plt.xlabel('X Coordinate')
-    plt.ylabel('Y Coordinate')
-    plt.title('Villes')
-    plt.show()
+    def draw_path(self, individu):
+        self.scene.clear()
+        pen = QPen(Qt.blue, 2)
+        brush = QBrush(Qt.red)
 
-villes= initailisation_des_villes(10)
-plot_data(villes)
+        coords = [(ville.x * 10, ville.y * 10) for ville in individu]
+        for x, y in coords:
+            self.scene.addEllipse(x-5, y-5, 10, 10, pen, brush)
+        for i in range(len(coords) - 1):
+            self.scene.addLine(coords[i][0], coords[i][1], coords[i+1][0], coords[i+1][1], pen)
+        self.scene.addLine(coords[-1][0], coords[-1][1], coords[0][0], coords[0][1], pen)
 
+class GeneticAlgorithmGUI(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Évolution des Itinéraires")
+        self.setGeometry(300, 300, 1080, 720)
+        
+        main_layout = QVBoxLayout()
 
-# import tkinter as tk
-# from tkinter import ttk
-# import matplotlib.pyplot as plt
+        # Zone de saisie
+        input_layout = QHBoxLayout()
+        self.entry_villes = QLineEdit(self)
+        self.entry_population = QLineEdit(self)
+        self.start_button = QPushButton("Démarrer", self)
+        self.start_button.clicked.connect(self.run_algorithm)
+        
+        input_layout.addWidget(QLabel("Nombre de villes:"))
+        input_layout.addWidget(self.entry_villes)
+        input_layout.addWidget(QLabel("Taille de la population:"))
+        input_layout.addWidget(self.entry_population)
+        input_layout.addWidget(self.start_button)
+        
+        main_layout.addLayout(input_layout)
 
-# def afficher_graphique():
-#     valeur = float(entree_valeur.get())
-#     x = [0, 1]
-#     y = [0, valeur]
+        # Zone d'affichage
+        self.graphics_view = GraphicsView(self)
+        main_layout.addWidget(self.graphics_view)
 
-#     plt.plot(x, y, marker='o')
-#     plt.title("Valeur saisie par l'utilisateur")
-#     plt.xlabel("Index")
-#     plt.ylabel("Valeur")
-#     plt.grid(True)
-#     plt.show()
+        self.setLayout(main_layout)
+        self.villes = []
+        self.population = None
+        self.generation = 0
 
-# # Créer la fenêtre principale
-# fenetre = tk.Tk()
-# fenetre.title("Interface Utilisateur")
+    def update_generation(self):
+        if self.generation < 100:
+            parents = selection(self.population)
+            enfants = recombinaison(parents)
+            enfants = mutation(enfants)
+            self.population = formation(self.population, enfants)
+            
+            best_individu = min(self.population, key=total_distance)
+            self.graphics_view.draw_path(best_individu)
+            self.generation += 1
+            QTimer.singleShot(500, self.update_generation)
+        else:
+            QMessageBox.information(self, "Fin", "L'algorithme a terminé son exécution.")
 
-# # Créer un champ d'entrée pour la valeur
-# label_valeur = ttk.Label(fenetre, text="Veuillez entrer une valeur :")
-# label_valeur.pack(pady=5)
+    def run_algorithm(self):
+        try:
+            num_villes = int(self.entry_villes.text())
+            taille_population = int(self.entry_population.text())
+        except ValueError:
+            QMessageBox.critical(self, "Erreur", "Veuillez entrer des valeurs numériques valides.")
+            return
+        
+        self.villes = initialisation_des_villes(num_villes)
+        self.population = [random.sample(self.villes, len(self.villes)) for _ in range(taille_population)]
+        self.generation = 0
+        self.update_generation()
 
-# entree_valeur = ttk.Entry(fenetre)
-# entree_valeur.pack(pady=5)
-
-# # Créer un bouton pour afficher le graphique
-# bouton_afficher = ttk.Button(fenetre, text="Afficher le graphique", command=afficher_graphique)
-# bouton_afficher.pack(pady=10)
-
-# # Lancer la boucle principale de l'interface utilisateur
-# fenetre.mainloop()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = GeneticAlgorithmGUI()
+    window.show()
+    sys.exit(app.exec_())
